4a5d25675196a067986b9f0b9b7f62c6cd39842e
18ToNyStArK18 <bysanivedavyas@gmail.com>
2025-10-31 15:46:34 +0530

    flags added for communications

---
+++ inc/flags.h
@@ -0,0 +1,30 @@
+//create flags
+#define CREATE_REQ 1
+#define FILE_ALREADY_EXISTS 2
+#define FILE_CREATION_SUCCESS 3
+
+//Read flags
+#define READ_REQ_NS 4
+#define READ_REQ_SS 5
+#define FILE_DOESNT_EXIST 6
+#define READ_NS_SUCCESS 7
+#define READ_SS_SUCCESS 8
+#define READ_NS_FAIL 9
+#define READ_SS_FAIL 10
+
+//some basic flags
+#define VIEW 11
+#define INFO 12
+#define DELETE 13
+#define STREAM 14
+#define LIST 15
+#define ADDACCESS_r 16
+#define ADDACCESS_w 17
+#define REMACCESS 18
+#define EXEC 19
+#define UNDO 20
+
+//Write flags (need to think about the logic
+
+
+
