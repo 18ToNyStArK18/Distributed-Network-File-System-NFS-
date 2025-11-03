c7dca82afeb3420098907cef3381aa82fea20d01
18ToNyStArK18 <bysanivedavyas@gmail.com>
2025-11-03 22:53:33 +0530

    changed the flags and the client.c

--- cmn_inc.h
+++ cmn_inc.h
@@ -9,20 +9,21 @@ typedef struct Packet{
 //create flags
 #define CREATE_REQ 1
 #define FILE_ALREADY_EXISTS 2
-#define FILE_CREATION_SUCCESS 3
 
 //Read flags
 #define READ_REQ_NS 4
 #define READ_REQ_SS 5
 #define FILE_DOESNT_EXIST 6
-#define READ_NS_SUCCESS 7
-#define READ_SS_SUCCESS 8
 #define READ_NS_FAIL 9
 #define READ_SS_FAIL 10
 
 //some basic flags
 #define VIEW 11
+#define VIEW_DATA 111
+#define VIEW_END 112
 #define INFO 12
+#define INFO_DATA 121
+#define INFO_END 122
 #define DELETE 13
 #define STREAM 14
 #define LIST 15
@@ -37,3 +38,6 @@ typedef struct Packet{
 
 // Register storage ip and port
 #define REG_SS 22
+
+#define Success 67
+#define Fail 69
